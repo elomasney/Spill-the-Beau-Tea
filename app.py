6500762c4b5438627d8c039c1e765d4ab4e184b3
elomasney <eimearlomasney01@gmail.com>
2021-04-15 15:34:07 +0000

    Added Registration page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -28,6 +29,11 @@ def home():
 def get_products():
     products = mongo.db.products.find()
     return render_template("products.html", products=products)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
