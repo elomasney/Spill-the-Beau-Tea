96529c0dc8845feb903364abbf09678b95253427
elomasney <eimearlomasney01@gmail.com>
2021-04-26 12:40:04 +0000

    Added toggle class to favourites icon in products

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,12 @@
+//Toggles the tooltip on the repurchase icon in reviews
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
+//Collapsable favourites accordion menu
 $('.collapse').collapse()
+
+// Toggles favourites icon button on click
+$('.far.fa-heart').click(function () {
+    $('.far.fa-heart').toggleClass('far fa-heart fas fa-heart')
+});
