73d87e7b2572528b6e3f4d2281bc338ab33b8a22
elomasney <eimearlomasney01@gmail.com>
2021-05-03 13:03:27 +0000

    Added 'back to product' button on reviews page and updated search bar placeholder text

--- app.py
+++ app.py
@@ -390,7 +390,7 @@ def login():
             if check_password_hash(
                existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(
+                flash("Welcome, {}!".format(
                     request.form.get("username").capitalize()))
                 return redirect(
                     url_for("profile", username=session["user"],
