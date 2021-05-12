d88c09a76d064f92c07f00321dce861410d65f66
elomasney <eimearlomasney01@gmail.com>
2021-05-12 19:50:08 +0000

    Removed unnecessary buttons from product info page

--- app.py
+++ app.py
@@ -750,4 +750,4 @@ def internal_server(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
